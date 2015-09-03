7230cf06c6930a16c8be047e5d64c45f6230e4e4
matiasdg <matiasdanielgarcia@gmail.com>
2015-09-03 01:29:31 -0300

    Added postMessage: in progress

---
+++ js/postMessage.js
@@ -0,0 +1,21 @@
+/**
+ * IMPORTANTE
+ * Esto hay que mergearlo en main.js
+ * Hecho aparte para evitar conflictos si alguien estuvo modificando main.js
+ */
+
+$(document).ready(function(){
+
+$('#postMessageBtn').click(postMessage);
+
+
+});
+
+
+function postMessage(){
+    //Traigo todo el contenido del mensaje
+
+
+    //Obtengo el id_muro
+
+}
