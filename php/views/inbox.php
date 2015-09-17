e3a9e369c26bb11344219671f5f7551d3b8d9367
matiasdg <matiasdanielgarcia@gmail.com>
2015-09-16 23:33:34 -0300

    #BandejaDeEntrada Obtener inbox en proceso. #Bug fixed requires por require_once para que no se redeclare una clase

---
+++ php/views/inbox.php
@@ -0,0 +1,59 @@
+<?php
+require_once (dirname(__DIR__)."/services/InboxRepositoryService.php");
+require_once (dirname(__DIR__)."/domain/Session.php");
+
+$mysession = new Session();
+$mysession->initSession();
+
+$inboxRepo = new InboxRepositoryService();
+
+$userId = $_SESSION['id'];
+
+
+$conversations = $inboxRepo -> getConversationsByUserId($userId);
+
+
+
+$cont = 0;
+while($conversation = $conversations -> fetch_object())
+{
+    $cont++;
+
+    if($conversation -> id_usuario == $userId )
+    {
+        echo "<li class='list-group-item'>
+                    <div class='conversation-item'>
+                        <div class='avatar'>
+                            <img class='img-circle' src='http://lorempixel.com/200/200/people/".$cont."' alt=''/>
+                        </div>
+                        <div class='userName'>
+                            <p>".$conversation->prop_nombre_bandeja." ".$conversation->prop_apellido_bandeja."</p>
+                        </div>
+                        <div class='message-review'>
+                            <p><span><i class='glyphicon glyphicon-triangle-left'></i></span>". $conversation->contenido."</p>
+                        </div>
+                        <div class='message-date'>
+                            <p>".$conversation->fecha_alta."</p>
+                        </div>
+                    </div>
+                </li>";
+    }else{
+        echo "<li class='list-group-item'>
+                    <div class='conversation-item'>
+                        <div class='avatar'>
+                            <img class='img-circle' src='http://lorempixel.com/200/200/people' alt=''/>
+                        </div>
+                        <div class='userName'>
+                            <p>".$conversation->prop_nombre_bandeja." ".$conversation->prop_apellido_bandeja."</p>
+                        </div>
+                        <div class='message-review'>
+                            <p>".$conversation->contenido."</p>
+                        </div>
+                        <div class='message-date'>
+                            <p>".$conversation->fecha_alta."</p>
+                        </div>
+                    </div>
+                </li>";
+    }
+}
+?>
