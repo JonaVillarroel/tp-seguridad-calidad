7c778a894c8ad91abf9019ce5b899a36dfef5164
matiasdg <matiasdanielgarcia@gmail.com>
2015-09-21 12:02:19 -0300

    Eliminado UserControlAdmin.php. Fix footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,23 @@
 <footer>
-    <h5 class="text-center">Integrantes: </h5>
+    <h5 class="text-center" data-toggle="modal" data-target="#modalDesarrolladores"><strong>Desarrolladores</strong></h5>
     <h5 class="text-center">Seguridad y Calidad en Aplicaciones Web</h5>
     <h5 class="text-center">Departamento de Ingeniería e Investigaciones Tecnológicas - Universidad Nacional de la Matanza</h5>
 </footer>
+
+
+<div class="modal fade bs-example-modal-sm" id="modalDesarrolladores" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="exampleModalLabel">Desarrolladores</h4>
+            </div>
+            <div class="modal-body">
+                <p>García Matías.</p>
+                <p>León Manuel.</p>
+                <p>Magnoni Nicolas.</p>
+                <p>Villarroel Jonathan.</p>
+            </div>
+        </div>
+    </div>
+</div>
