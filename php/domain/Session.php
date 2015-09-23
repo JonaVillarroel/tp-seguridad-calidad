afdc9b2803b4ac029d41dff7a45e8a91ba3ab481
matiasdg <matiasdanielgarcia@gmail.com>
2015-09-22 21:40:35 -0300

    Fixes

--- php/domain/Session.php
+++ php/domain/Session.php
@@ -2,14 +2,6 @@
 
 //Para prevenir robo de sesion
 // Previene ataque xss para robar el identificador de sesion
-ini_set('session.cookie_httponly', 1);
-
-// **PREVENTING SESSION FIXATION**
-// Id de sesion no puede ser pasada por url
-ini_set('session.use_only_cookies', 1);
-
-// Usa una conexion segura (HTTPS) si es posible
-ini_set('session.cookie_secure', 1);
 
 
 	class Session{
