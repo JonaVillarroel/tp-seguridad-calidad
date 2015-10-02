97989f725a5be4edc8d55ad8420556240a146961
fedderamirezz <fedderamirezz@gmail.com>
2015-10-02 09:11:52 -0300

    sessionSecurityAgain

--- php/domain/Session.php
+++ php/domain/Session.php
@@ -8,16 +8,6 @@
 		public function __construct(){}
 		
 		public function initSession(){
-			//Para prevenir robo de sesion
-			// Previene ataque xss para robar el identificador de sesion
-			ini_set('session.cookie_httponly', 1);
-
-			// **PREVENTING SESSION FIXATION**
-			// Id de sesion no puede ser pasada por url
-			ini_set('session.use_only_cookies', 1);
-
-			// Usa una conexion segura (HTTPS) si es posible
-			ini_set('session.cookie_secure', 1);
 			@session_start();
 		}
 		
