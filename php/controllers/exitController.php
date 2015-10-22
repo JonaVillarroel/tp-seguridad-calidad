0f0946ef31cdc14f70cabd6443ee9eb0baa46a1c
Nico-Magnoni <niko_independiente@live.com.ar>
2015-10-22 16:34:45 -0300

    seguridad en sesion

--- php/controllers/exitController.php
+++ php/controllers/exitController.php
@@ -2,8 +2,16 @@
 	require '../domain/Session.php';
 	$mysession = new Session();
 	$mysession->initSession();
-	
+	//ANTI CSRF
+	 if (isset($_GET["csrf"]) && $_GET["csrf"] == $_SESSION["token"]) {
+        $_SESSION = array();
+     
 	$mysession->destroySession();
-	
+	}else{
+		header('location: ../../index.php?error=4');
+		exit();
+		 }
+
 	header('location: ../../index.php');
+	exit();
 ?>
